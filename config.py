5559bfb14de6456bdfd547fba5b7807489be158f
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2026-01-16 20:15:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+import re
+from os import environ
+import os
+
+id_pattern = re.compile(r'^.\d+$')
+
+API_ID = os.environ.get("API_ID", "10811400")
+API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6626666215:AAFSI_ZRp6aoTy9boDgxkrd_2PjyT4myeGg")
+ADMIN = int(os.environ.get("ADMIN", '6469754522'))
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://HARSHA24:HARSHA24@cluster0.sxaj8up.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
